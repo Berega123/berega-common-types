e70d3ad3e145ead06a1c8852815af87a93c1a3fa
Yurii Shekhanov <your.email@example.com>
2025-07-16 00:14:50 +0300

    Export RoomTypes as ApartmentRoomType for enhanced modularity.

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -8,3 +8,4 @@ export { PropertyTypes, DealTypes, StatusTypes } from './constants/property-type
 export { PropertyType as ApartmentPropertyType } from './constants/property-types';
 export { DealType as ApartmentDealType } from './constants/property-types';
 export { StatusType as ApartmentStatusType } from './constants/property-types';
+export { RoomTypes as ApartmentRoomType } from './constants/property-types';
--- dist/index.js
+++ dist/index.js
@@ -14,7 +14,7 @@ var __exportStar = (this && this.__exportStar) || function(m, exports) {
     for (var p in m) if (p !== "default" && !Object.prototype.hasOwnProperty.call(exports, p)) __createBinding(exports, m, p);
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.StatusTypes = exports.DealTypes = exports.PropertyTypes = void 0;
+exports.ApartmentRoomType = exports.StatusTypes = exports.DealTypes = exports.PropertyTypes = void 0;
 // Export all interfaces from the package
 __exportStar(require("./interfaces/IApartment"), exports);
 __exportStar(require("./interfaces/ICatalog"), exports);
@@ -27,3 +27,5 @@ var property_types_1 = require("./constants/property-types");
 Object.defineProperty(exports, "PropertyTypes", { enumerable: true, get: function () { return property_types_1.PropertyTypes; } });
 Object.defineProperty(exports, "DealTypes", { enumerable: true, get: function () { return property_types_1.DealTypes; } });
 Object.defineProperty(exports, "StatusTypes", { enumerable: true, get: function () { return property_types_1.StatusTypes; } });
+var property_types_2 = require("./constants/property-types");
+Object.defineProperty(exports, "ApartmentRoomType", { enumerable: true, get: function () { return property_types_2.RoomTypes; } });
